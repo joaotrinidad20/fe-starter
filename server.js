421dd4497fd9009d29a7c61206f42fd9f737cb11
joaotrinidad <73096152+joaotrinidad@users.noreply.github.com>
2022-02-17 15:39:23 +0800

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const express = require("express");
 const server = express();
-const PORT = process.env.PORt || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.use(express.static("public"));
 
